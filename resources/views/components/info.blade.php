893f889f77a4ecd2b0f04dbd3b2b8fedb2262c82
tribber93 <tribberyoni5@gmail.com>
2023-05-27 11:21:55 +0700

    tambah login page dan memperbaiki home page

--- resources/views/components/info.blade.php
+++ resources/views/components/info.blade.php
@@ -8,8 +8,8 @@
             </div>
         </div>
 
-        <div class="row d-flex">
-            <div class="col-12 mb-5">
+        <div class="row d-flex justify-content-center">
+            <div class="col-md-12 mb-5 d-none d-md-block">
                 <button class="cta pe-lg-0 pe-5">
                     <span class="hover-underline-animation"> Lainnya </span>
                     <svg viewBox="0 0 46 16" height="10" width="30" xmlns="http://www.w3.org/2000/svg"
@@ -101,6 +101,17 @@
                             </div>
                         </div>
                     </div>
+                </div>
+                <div class="d-flex justify-content-center d-md-none">
+                    <button class="cta mt-3">
+                        <span class="hover-underline-animation"> Lainnya </span>
+                        <svg viewBox="0 0 46 16" height="10" width="30" xmlns="http://www.w3.org/2000/svg"
+                            id="arrow-horizontal">
+                            <path transform="translate(30)"
+                                d="M8,0,6.545,1.455l5.506,5.506H-30V9.039H12.052L6.545,14.545,8,16l8-8Z"
+                                data-name="Path 10" id="Path_10"></path>
+                        </svg>
+                    </button>
                 </div>
                 <!-- End Single Blog -->
             </div>
