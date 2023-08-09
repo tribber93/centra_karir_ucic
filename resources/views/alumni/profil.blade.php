92086e4bb8ebb881e35ec181c037b1b2461b872d
tribber93 <tribberyoni5@gmail.com>
2023-08-09 13:12:19 +0700

    profile

---
+++ resources/views/alumni/profil.blade.php
@@ -0,0 +1,102 @@
+@extends('layouts.user_admin')
+@section('judul', 'Forum Diskusi')
+@section('konten')
+    <div class="container m-3">
+        <div class="box">
+            <div class="box-header">
+                <h2>Data Alumni</h2>
+            </div>
+            <form action="">
+                <table class="table table-striped b-t">
+                    <thead>
+                        <img class="p-4" src="https://upload.wikimedia.org/wikipedia/commons/2/24/Circle-icons-image.svg"
+                            alt="" height="200px">
+                        <input type="file" id="exampleInputFile" class="form-control">
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Nama</td>
+                            <td>:</td>
+                            <td colspan="5">{{ $alumni->nama_alumni }}</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td>Angkatan</td>
+                            <td>:</td>
+                            <td colspan="5">{{ $alumni->angkatan }}</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td>Jenjang</td>
+                            <td>:</td>
+                            <td colspan="5">{{ $alumni->jenjang }}</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td>Program Studi</td>
+                            <td>:</td>
+                            <td colspan="5">{{ $alumni->prodi }}</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td>NIM</td>
+                            <td>:</td>
+                            <td colspan="5">{{ $alumni->nim }}</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td>Tahun Lulus</td>
+                            <td>:</td>
+                            <td colspan="5">{{ $alumni->tahun_lulus }}</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td>Nomor Telepon</td>
+                            <td>:</td>
+                            <td colspan="5">
+                                <input type="text" id="noTelp" value="{{ $alumni->no_telpon }}" name="noTelp"
+                                    class="form-control" placeholder="{{ $alumni->no_telpon }}">
+                            </td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td>Testimoni</td>
+                            <td>:</td>
+                            <td colspan="5">
+                                <input type="text" id="testimoniAlumni" name="testimoniAlumni" class="form-control"">
+                            </td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </form>
+
+        </div>
+    </div>
+    </div>
+    </div>
+@endsection
